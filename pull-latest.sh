71212644377965f5794120c3235e4eec6828ab76
veqox <sonnleitner.luki@gmail.com>
2024-03-03 01:48:54 +0100

    feat: add script to pull latest images

---
+++ pull-latest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker pull ghcr.io/ao-analytics/ao-analytics-frontend:main
+docker pull ghcr.io/ao-analytics/ao-analytics-api:main
+docker pull ghcr.io/ao-analytics/ao-analytics-db:main
+docker pull ghcr.io/ao-analytics/ao-analytics-nats:main
+docker pull ghcr.io/ao-analytics/ao-analytics-migrator:main
