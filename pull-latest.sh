08ec6b0671eab38ea17d57c4618a2a39e12a82da
veqox <sonnleitner.luki@gmail.com>
2024-03-12 10:45:42 +0100

    fix: update image tags

--- pull-latest.sh
+++ pull-latest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker pull ghcr.io/ao-analytics/ao-analytics-frontend:main
-docker pull ghcr.io/ao-analytics/ao-analytics-api:main
-docker pull ghcr.io/ao-analytics/ao-analytics-db:main
-docker pull ghcr.io/ao-analytics/ao-analytics-nats:main
-docker pull ghcr.io/ao-analytics/ao-analytics-migrator:main
+docker pull ghcr.io/ao-analytics/ao-analytics-frontend:latest
+docker pull ghcr.io/ao-analytics/ao-analytics-api:latest
+docker pull ghcr.io/ao-analytics/ao-analytics-db:latest
+docker pull ghcr.io/ao-analytics/ao-analytics-nats:latest
+docker pull ghcr.io/ao-analytics/ao-analytics-migrator:latest
