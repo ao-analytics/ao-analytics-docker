c35829b25fe9cdecc04ed5a1dd813beb70e8f5c7
veqox <sonnleitner.luki@gmail.com>
2024-04-05 23:18:31 +0200

    refactor: update compose file for timescale

--- pull-latest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker pull ghcr.io/ao-analytics/ao-analytics-frontend:latest
-docker pull ghcr.io/ao-analytics/ao-analytics-api:latest
-docker pull ghcr.io/ao-analytics/ao-analytics-db:latest
-docker pull ghcr.io/ao-analytics/ao-analytics-nats:latest
-docker pull ghcr.io/ao-analytics/ao-analytics-migrator:latest
