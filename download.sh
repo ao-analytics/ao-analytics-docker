39a665664f21663aa92c0918f3d51325b0f8d444
veqox <sonnleitner.luki@gmail.com>
2024-04-24 17:02:26 +0200

    feat: configure services with env variables

--- download.sh
+++ download.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir -p data/migrator
-
-curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/world.json > data/migrator/locations.json
-curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/items.json > data/migrator/items.json
-curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/items.json > data/migrator/localizations.json
+curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/world.json > migrator/locations.json
+curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/items.json > migrator/items.json
+curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/items.json > migrator/localizations.json
