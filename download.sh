529e0fd8595c2137add757c6143155b8d268fff7
veqox <sonnleitner.luki@gmail.com>
2024-06-15 18:10:49 +0200

    feat: run services behind nginx feat: add compose file for local development

--- download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/world.json > migrator/locations.json
-curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/items.json > migrator/items.json
-curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/items.json > migrator/localizations.json
